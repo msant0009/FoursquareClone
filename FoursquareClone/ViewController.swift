1870c68bd7b880d3cd1d7636ea418b64b600c5c9
Mark Santoro <mark_santoro@comcast.net>
2024-08-09 14:51:39 -0700

    add and test Parse read capability

--- FoursquareClone/ViewController.swift
+++ FoursquareClone/ViewController.swift
@@ -9,26 +9,43 @@ import UIKit
 import ParseCore
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-        let parseObject = PFObject(className: "Fruits")
-        parseObject["name"] = "Apple"
-        parseObject["Calories"] = 100
-        parseObject.saveInBackground { (Success, error) in
+        //        let parseObject = PFObject(className: "Fruits")
+        //        parseObject["name"] = "Banana"
+        //
+        //        parseObject["Calories"] = 150
+        //        parseObject.saveInBackground { (Success, error) in
+        //            if error != nil {
+        //                print(error?.localizedDescription)
+        //            } else{
+        //                print("uploaded")
+        //
+        //            }
+        //        }
+        //
+        //
+        
+        let query = PFQuery(className: "Fruits")
+     //   query.whereKey("name", equalTo: "Apple")
+     //   query.whereKey("calories", greaterThan: 120)
+        query.findObjectsInBackground { (objects, error) in
             if error != nil {
                 print(error?.localizedDescription)
             } else{
-                print("uploaded")
-                
+                print(objects)
+            
             }
         }
         
+
+        
+        
+        
         
     }
-
-
 }
 
