8ecf735c226d6e8c99d07d892e8b7d40c9b34354
Mark Santoro <mark_santoro@comcast.net>
2024-08-10 16:51:03 -0700

    addPlaceVC added to main storyboard

---
+++ FoursquareClone/AddPlaceVC.swift
@@ -0,0 +1,32 @@
+//
+//  AddPlaceVC.swift
+//  FoursquareClone
+//
+//  Created by Mark Santoro on 8/10/24.
+//
+
+import UIKit
+
+class AddPlaceVC: UIViewController {
+
+    @IBOutlet var placeNameText: UITextField!
+   
+    @IBOutlet var placeTypeText: UITextField!
+    
+    @IBOutlet var placeAtmosphereText: UITextField!
+    
+  
+    @IBOutlet var placeImageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    
+    @IBAction func nextButtonClicked(_ sender: Any) {
+    }
+    
+}
