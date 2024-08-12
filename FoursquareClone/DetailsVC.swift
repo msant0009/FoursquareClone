76a75b6f1123eb735554eadbf21496557152b27b
Mark Santoro <mark_santoro@comcast.net>
2024-08-12 08:55:06 -0700

    add details VC

---
+++ FoursquareClone/DetailsVC.swift
@@ -0,0 +1,32 @@
+//
+//  DetailsVC.swift
+//  FoursquareClone
+//
+//  Created by Mark Santoro on 8/12/24.
+//
+
+import UIKit
+import MapKit
+
+class DetailsVC: UIViewController {
+
+    @IBOutlet var detailsImageView: UIImageView!
+    
+    @IBOutlet var detailsNameLabel: UILabel!
+    
+    @IBOutlet var detailsTypeLabel: UILabel!
+    
+    @IBOutlet var detailsAtmosphereLabel: UILabel!
+ 
+    @IBOutlet var detailsMapView: MKMapView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    
+
+}
