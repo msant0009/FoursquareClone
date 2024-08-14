1aa6e81406e6e9b0b28c5f62a1d93d56d5c4f79b
Mark Santoro <mark_santoro@comcast.net>
2024-08-14 15:34:06 -0700

    map info added

--- FoursquareClone/DetailsVC.swift
+++ FoursquareClone/DetailsVC.swift
@@ -43,7 +43,7 @@ class DetailsVC: UIViewController {
                 if objects != nil {
                     if objects!.count > 0 {
                         let chosenPlaceObject = objects![0]
-                        
+                        // OBJECTS
                         if let placeName = chosenPlaceObject.object(forKey: "name") as? String {
                             self.detailsNameLabel.text = placeName
                         }
@@ -75,7 +75,20 @@ class DetailsVC: UIViewController {
                                 }
                             }
                         }
+                        // MAPS
+                        let location = CLLocationCoordinate2D(latitude: self.chosenLatitude, longitude: self.chosenLongitude)
                         
+                        let span = MKCoordinateSpan(latitudeDelta: 0.035, longitudeDelta: 0.035)
+                        
+                        let region = MKCoordinateRegion(center: location, span: span)
+                        
+                        self.detailsMapView.setRegion(region, animated: true)
+                        
+                        let annotation = MKPointAnnotation()
+                        annotation.coordinate = location
+                        annotation.title = self.detailsNameLabel.text
+                        annotation.subtitle = self.detailsTypeLabel.text
+                        self.detailsMapView.addAnnotation(annotation)
                         
                     }
                     
